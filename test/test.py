416484399d6c9cfdcdf45ccc75219b4444eeb9eb
WhatAreYouSaying <360307598@qq.com>
2017-04-23 22:00:16 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -29,3 +29,13 @@ class AnswerTestCase(unittest.TestCase):
         data = Answer(url="https://www.zhihu.com/question/19761434/answer/14005147").vote_neutral()
         self.assertIn("voting", data)
         self.assertIn("voteup_count", data)
+
+        
+class ArticleTestCase(unittest.TestCase):
+    def test_vote_up_with_id(self):
+        time.sleep(1)
+        data=Article(id="26415908")
+        
+        #self.assertIn("like",data)
+
+        #assertIn这边还没有很清楚
