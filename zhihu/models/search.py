595abaaf763d2c0bb1a54b2369fa7b4260d74d72
Pan <zipan@paypal.com>
2017-04-26 03:32:40 +0800

    add docs

--- zhihu/models/search.py
+++ zhihu/models/search.py
@@ -10,6 +10,13 @@ from zhihu.url import URL
 class Search(Model):
     @need_login
     def search_content(self, q=''):
+        """
+        搜索内容
+        :param q: 搜索关键词
+        :return: (ids, titles) 问题id与题目
+        >>> search = zhihu.Search()
+        >>> search.search_content('python')
+        """
         html = self._get_search_response(type='content', q=q)
         return self._extract_questions(html)
 
