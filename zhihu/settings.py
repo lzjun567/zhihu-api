c55725472bfc8ee72a7b321a670a17ec965eed75
Oopswc <706543191@qq.com>
2017-04-28 21:25:57 +0800

    add UserAgent list
    for random selection

--- zhihu/settings.py
+++ zhihu/settings.py
@@ -1,7 +1,52 @@
 # encoding: utf-8
 import os.path
+import random
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87'
+#UserAgent list
+user_agent_list = [
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87"
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "  
+        "(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",  
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 "  
+        "(KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 "  
+        "(KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 "  
+        "(KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",  
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 "  
+        "(KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",  
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 "  
+        "(KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",  
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 "  
+        "(KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",  
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",  
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 "  
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",  
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 "  
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"  
+       ]  
+
+#Select UA by random
+USER_AGENT = random.choice(user_agent_list)
+
+print(USER_AGENT)
 
 HEADERS = {"Host": "www.zhihu.com",
            "Referer": "https://www.zhihu.com/",
