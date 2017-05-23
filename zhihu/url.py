93d88ded3ea429a1010a14e0736f9a23a88dc8f4
Pan <zipan@paypal.com>
2017-05-23 23:34:00 +0800

    return the correct search url

--- zhihu/url.py
+++ zhihu/url.py
@@ -107,5 +107,5 @@ class URL(object):
 
     # 搜索内容／用户／话题
     @staticmethod
-    def search():
-        return URL.host + "/search"
+    def search(get_more=False):
+        return URL.host + "/search" if get_more else URL.host + "/r/search"
