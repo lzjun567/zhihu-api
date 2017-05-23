643b4b14dac4baffdce20752ca3c2cda1d4eb33d
Pan <zipan@paypal.com>
2017-05-23 23:34:14 +0800

    change variable name

--- zhihu/models/search.py
+++ zhihu/models/search.py
@@ -9,15 +9,15 @@ from zhihu.url import URL
 
 class Search(Model):
     @need_login
-    def search_content(self, q=''):
+    def search_content(self, key_words=''):
         """
         搜索内容
-        :param q: 搜索关键词
+        :param key_words: 搜索关键词
         :return: (ids, titles) 问题id与题目
         >>> search = zhihu.Search()
         >>> search.search_content('python')
         """
-        html = self._get_search_response(search_type='content', key_words=q)
+        html = self._get_search_response(search_type='content', key_words=key_words)
         return self._extract_questions(html)
 
     def _get_search_response(self, search_type='content', key_words=''):
@@ -28,6 +28,6 @@ class Search(Model):
     def _extract_questions(html):
         soup = BeautifulSoup(html, 'html.parser')
         ul = soup.find('ul', {'class': ['list', 'contents', 'navigable']})
-        ids = [li.div.a['href'][10:] for li in ul.findChildren(recursive=False)]
-        titles = [li.div.a.getText() for li in ul.findChildren(recursive=False)]
+        ids = [li.div.a['href'][10:] for li in ul.findChildren(recursive=False) if "question" in li.div.a['href'][10:]]
+        titles = [li.div.a.getText() for li in ul.findChildren(recursive=False) if "question" in li.div.a['href'][10:]]
         return ids, titles
