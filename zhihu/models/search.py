e0c4248c5ece2121af1ca933d9c0f1a331822b2e
Pan <zipan@paypal.com>
2017-05-24 00:13:31 +0800

    increase offset at the end of the loop

--- zhihu/models/search.py
+++ zhihu/models/search.py
@@ -28,6 +28,7 @@ class Search(Model):
             new_ids, new_titles = self._extract_questions(html, get_more=True)
             ids += new_ids
             titles += new_titles
+            offset += 10
         return ids[:number_of_results], titles[:number_of_results]
 
     def _get_search_response(self, search_type='content', key_words='', get_more=False, offset=0):
