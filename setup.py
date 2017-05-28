6bf6a2568e9179728654d54cdbbea10c68fb7e1a
liuzhijun <lzjun567@qq.com>
2017-05-29 07:31:05 +0800

    udpate setup

--- setup.py
+++ setup.py
@@ -1,14 +1,46 @@
 from setuptools import setup
+import zhihu
+from setuptools import find_packages
+import io
+
+with io.open("README.md", encoding="utf8") as f:
+    readme = f.read()
 
 setup(
     name="zhihu-api",
-    version="",
-
-    packages=[
-        "zhihu",
-        "zhihu.models"
-    ],
+    version=zhihu.__version__,
+    author=zhihu.__author__,
+    author_email="lzjun567@gmail.com",
+    url="https://github.com/lzjun567/zhihu-api",
+    packages=find_packages(),
+    keyworads = "zhihu",
+    description="zhihu api from humans",
+    long_description=readme,
+    # packages=[
+    #     "zhihu",
+    #     "zhihu.models"
+    # ],
     include_package_data=True,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 0 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+
     install_requires=[
 
         "pytest-runner",
