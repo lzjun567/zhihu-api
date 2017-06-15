1b0004ab3219fb6f1610a9fdb59c945af0a6a309
kylin60 <759147983@qq.com>
2017-06-15 18:41:01 +0800

    ...

--- zhihu/models/__init__.py
+++ zhihu/models/__init__.py
@@ -76,6 +76,7 @@ class Model(object):
         :param kwargs:
         :return:
         """
+        print(kwargs)
         response = self._session.get(url or URL.index(), **kwargs)
         soup = BeautifulSoup(response.content, "html.parser")
         xsrf = soup.find('input', attrs={"name": "_xsrf"}).get("value")
