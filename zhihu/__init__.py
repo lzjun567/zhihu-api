a56a44fb89ca8871f0e1208127752267b733cf12
liuzhijun <lzjun567@qq.com>
2017-07-30 00:00:31 +0800

    Fix error

--- zhihu/__init__.py
+++ zhihu/__init__.py
@@ -1,16 +1,16 @@
-# from zhihu.models import answer
-# from zhihu.models import zhihux
-# from zhihu.models import question
-# from zhihu.models import column
-# from zhihu.models import account
-#
-# __author__ = 'liuzhijun'
-# __license__ = 'MIT'
-#
-# __all__ = ["Answer", "Zhihu", "Question", "Column", "Account"]
-#
-# Answer = answer.Answer
-# Zhihu = zhihux.ZhihuX
-# Question = question.Question
-# Column = column.Column
-# Account = account.Account
+from .models import answer
+from .models import zhihu
+from .models import question
+from .models import column
+from .models import account
+
+__author__ = 'liuzhijun'
+__license__ = 'MIT'
+
+__all__ = ["Answer", "Zhihu", "Question", "Column", "Account"]
+
+Answer = answer.Answer
+Zhihu = zhihu.Zhihu
+Question = question.Question
+Column = column.Column
+Account = account.Account
